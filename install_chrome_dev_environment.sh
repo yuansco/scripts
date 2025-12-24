a263f2cc4ad72a2df4d7bb19d9cfdac59f4ae959
Yu-An <yu-an.chen@quanta.corp-partner.google.com>
2025-12-24 11:13:59 +0800

    update

--- install_chrome_dev_environment.sh
+++ install_chrome_dev_environment.sh
@@ -199,7 +199,7 @@ fi
 
 LOG "Detect OS version: $DISTRIB_ID $DISTRIB_RELEASE"
 
-if [[ "$DISTRIB_ID" != "Ubuntu" || "$DISTRIB_RELEASE" != "24.04" ]]; then
+if [[ "$DISTRIB_ID" != "Ubuntu" || ("$DISTRIB_RELEASE" != "24.04" && "$DISTRIB_RELEASE" != "22.04") ]]; then
 
     # this script only verified on Ubuntu 24.04 LTS,
     # show the warning message if run in not verified version.
@@ -858,17 +858,41 @@ then
     #  Install Docker Engine on Ubuntu
     for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-    # Add Docker's official GPG key:
-    sudo apt-get update
-    sudo apt-get install -y ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    # get os name and version
+    if [[ "$DISTRIB_ID" == "" ]]; then
+        source /etc/lsb-release
+    fi
 
-    # Add the repository to Apt sources:
-    echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    if [[ "$DISTRIB_ID" == "Ubuntu" && "$DISTRIB_RELEASE" == "22.04" ]]; then
+
+        # For Ubuntu 22.04
+        # Add Docker's official GPG key:
+        sudo apt-get update
+        sudo apt-get install -y ca-certificates curl gnupg
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+        # Add the repository to Apt sources:
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    else
+        # For default Ubuntu 24.04
+        # Add Docker's official GPG key:
+        sudo apt-get update
+        sudo apt-get install -y ca-certificates curl
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+        # Add the repository to Apt sources:
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    fi
+
+    # Add the repository to Apt sources(continue):
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
